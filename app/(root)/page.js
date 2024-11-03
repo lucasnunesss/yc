731c61447d7e6a37a6af1c8131fec576a8d51bfe
Lucas Nunes <lucas.jlnunes@gmail.com>
2024-11-03 16:17:34 -0300

    nothing too special

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+
 import { STARTUPS_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 
 export default async function Home({searchParams}) {
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY);
- 
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY})
   return (
     <>
      <section className="pink_container">
@@ -36,6 +36,8 @@ export default async function Home({searchParams}) {
           )}
         </ul>
      </section>
+
+     <SanityLive />
     </>
   );
 }
