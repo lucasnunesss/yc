68acad5de41fe7eaee3108c98de18b1dc997f723
Lucas Nunes <lucas.jlnunes@gmail.com>
2024-11-03 16:28:24 -0300

    add search and filtered data

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -8,7 +8,8 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({searchParams}) {
   const query = (await searchParams).query;
-  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY})
+  const params = {search: query || null}
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params})
   return (
     <>
      <section className="pink_container">
