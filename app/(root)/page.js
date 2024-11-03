ecf8cd2441f50a1c5fdcc9557e53f5814978ce1a
Lucas Nunes <lucas.jlnunes@gmail.com>
2024-11-03 11:57:07 -0300

    Finish version of fake data

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,9 +1,20 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 
 export default async function Home({searchParams}) {
-  const query = (await searchParams).query
+  const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: {_id: 1, name: "Paul McCartney"},
+    _id: 1,
+    description: "This is a description",
+    image: "https://i.imgur.com/uJJjsJ8.jpeg",
+    category: "Music",
+    title: "John Lennon"
+  }]
   return (
     <>
      <section className="pink_container">
@@ -14,6 +25,22 @@ export default async function Home({searchParams}) {
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
 
         <SearchForm query={query} />
+     </section>
+
+     <section className="section_container">
+        <p className="text-30-semibold">
+            {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post, index) => (
+                <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
      </section>
     </>
   );
