45eebf72526d8edd27ec483e91d7a3c286643683
Lucas Nunes <lucas.jlnunes@gmail.com>
2024-11-05 06:11:52 -0300

    add ui, add next in override and add one more criew to views

---
+++ components/View.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Ping from './Ping'
+import { client } from '@/sanity/lib/client'
+import { STARTUP_VIEWS_QUERY } from '@/lib/queries'
+import { formViews } from '@/lib/utils'
+
+// Parte dinamica
+const View = async({id}) => {
+  const {views: totalViews} = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {id})
+  return (
+    <div className='view-container'>
+        <div className='absolute -top-2 -right-2'>
+          <Ping />
+        </div>
+
+        <p className='view-text'>
+            <span className='font-black'>{formViews(totalViews)}</span>
+        </p>
+    </div>
+  )
+}
+
+export default View
