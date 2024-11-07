202e18d57c7c4243e9a77f671d0c8b15bc640ab0
Lucas Nunes <lucas.jlnunes@gmail.com>
2024-11-07 07:08:40 -0300

    add validation fields, attention to link field

--- components/StartupForm.jsx
+++ components/StartupForm.jsx
@@ -6,12 +6,16 @@ import MDEditor from '@uiw/react-md-editor'
 import { Button } from './ui/button'
 import { Send } from 'lucide-react'
 import { useForm } from 'react-hook-form'
+import { isValidUrl, warningMsg } from '@/lib/utils'
 const StartupForm = () => {
 
   const [pitch, setPitch] = useState("")
   const form = useForm({
     defaultValues: {
-      title: ""
+      title: "",
+      description: "",
+      category: "",
+      link: ""
     }
   });
 
@@ -24,7 +28,7 @@ const StartupForm = () => {
     
   })
   const handleFormSubmit = (data) => {
-      setErrors({...errors, title: errorState?.title?.type})
+     console.log("oioioi")
 
   
   }
@@ -45,39 +49,78 @@ const StartupForm = () => {
           required: true,
           minLength: {
             value: 3,
-            message: "Small title"
+            message: warningMsg("Title", true, false)
           },
 
           maxLength: {
             value: 100,
-            message: "Title to big"
+            message: warningMsg("Title", false, true)
           },
         })} />
 
-        {errorState.title && <p className='startup-form_error'>{errorState.title?.message}</p>}
+        {errorState?.title && <p className='startup-form_error'>{errorState.title?.message}</p>}
       </div>
 
       <div>
         <label htmlFor="description" className='startup-form_label'>Description</label>
-        <Textarea id="description" name="description" className="startup-form_textarea" required placeholder="Startup Description" />
+        <Textarea id="description" name="description" className="startup-form_textarea" required placeholder="Startup Description" {...register("description", {
+          required: true,
+            minLength: {
+              value: 20,
+              message: warningMsg("Description", true, false)
+            },
+            maxLength: {
+              value: 500,
+              message: warningMsg("Description", false, true)
+            }
+        })} />
 
-        {errors.description && <p className='startup-form_error'>{errors.description}</p>}
+        {errorState?.description && <p className='startup-form_error'>{errorState?.description?.message}</p>}
       </div>
 
 
       <div>
         <label htmlFor="category" className='startup-form_label'>Category</label>
-        <Input id="category" name="category" className="startup-form_input" required placeholder="Startup Category (Tech, Health, Education...)" />
+        <Input id="category" name="category" className="startup-form_input" required placeholder="Startup Category (Tech, Health, Education...)" 
+          {...register("category", {
+            required: true,
+            minLength: {
+              value: 3,
+              message: warningMsg("Category", true, false)
+            },
+            maxLength: {
+              value: 20,
+              message: warningMsg("Category", false, true)
+            }
+          })}
+        />
 
-        {errors.category && <p className='startup-form_error'>{errors.category}</p>}
+        {errorState?.category && <p className='startup-form_error'>{errorState?.category?.message}</p>}
       </div>
 
 
       <div>
         <label htmlFor="link" className='startup-form_label'>Image URL</label>
-        <Input id="link" name="link" className="startup-form_input" required placeholder="Startup Image URL" />
+        <Input id="link" name="link" className="startup-form_input" required placeholder="Startup Image URL" {...register("link", {
+          validate: async(result) => {
+              if(isValidUrl(result) === false){
+                return "This is not an image"
+              }
+              
+              try {
+                const res = await fetch(result)
+                const constentType = res?.headers?.get("content-type");
+                if(!constentType?.startsWith('image/')){
+                    return "This is not an image"
+                }
+              } catch (error) {
+                  return "This is not an image"
+              }
+    
+          } 
+        })} />
 
-        {errors.link && <p className='startup-form_error'>{errors.link}</p>}
+        {errorState?.link && <p className='startup-form_error'>{errorState?.link?.message}</p>}
       </div>
 
       <div data-color-mode="light">
@@ -98,7 +141,7 @@ const StartupForm = () => {
         {errors.pitch && <p className='startup-form_error'>{errors.pitch}</p>}
       </div>
 
-      <Button type="submit" className="startup-form_btn text-white" disabled={isPending} >
+      <Button type="submit" className="startup-form_btn text-white"  >
         {isPending ? 'Submitting...' : 'Submit your pitch'}
             <Send className='size-6 ml-2' />
       </Button>
