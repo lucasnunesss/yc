cfe8ae1dcb7690ce49d0011b40a7ea91a81e0e01
Lucas Nunes <lucas.jlnunes@gmail.com>
2024-11-08 12:09:56 -0300

    init basic page

---
+++ app/(root)/user/[id]/page.jsx
@@ -0,0 +1,55 @@
+import { auth } from '@/auth';
+import { AUTHOR_BY_GITHUB_ID_QUERY, AUTHOR_BY_ID_QUERY } from '@/lib/queries';
+import { client } from '@/sanity/lib/client';
+import { notFound } from 'next/navigation';
+import React from 'react'
+import Image from 'next/image';
+
+export const experimental_ppr = true;
+
+const Page = async({params}) => {
+  const id = (await params).id;
+  const session = await auth();
+
+  const user = await client.fetch(AUTHOR_BY_ID_QUERY, {id})
+
+  if(!user) return notFound()
+  return (
+    <>
+      <section className='profile_container'>
+        <div className='profile_card'>
+          <div className='profile_title'>
+            <h3 className='text-24-black uppercase text-center line-clamp-1'>
+              {user.name}
+              </h3>
+          </div>
+
+          <Image src={user.image} alt={user.name}
+            width={220}
+            height={220}
+            className='profile_image'
+          />
+        <p className='text-30-extrabold mt-7 text-center'>
+            @{user?.username}
+        </p>
+        <p className='mt-1 text-center text-14-normal'>{user?.bio}</p>
+      </div>
+
+      <div className='flex-1 flex flex-col gap-5 lg:-mt-5'>
+        <p className='text-30-bold'>
+          {session?.id === id ? "Your" : "All"} Startups
+        </p>
+
+      <ul className='card_grid-sm'>
+          {/* TODO: ADD USER_STARTUPS 
+           -- PRECISA SER DINAMICO AQUI */}
+      </ul>
+
+      </div>
+
+      </section>
+    </>
+  )
+}
+
+export default Page
